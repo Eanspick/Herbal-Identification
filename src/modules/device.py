aa869bd51fe894293f0958c4b0d24ced0181a7ba
Eanspick <ericanepomuceno92@gmail.com>
2024-07-08 03:13:11 +0800

    Update model

--- src/modules/device.py
+++ src/modules/device.py
@@ -1,4 +1,10 @@
+from typing import overload
+
 import torch
+import torch.nn as nn
+from typing_extensions import TypeVar
+
+T = TypeVar("T", bound=torch.Tensor | nn.Module)
 
 
 def get_default_device():
@@ -10,3 +16,20 @@ def get_default_device():
 
 
 DEVICE = get_default_device()
+
+
+@overload
+def to_device(
+    tensor: list[T] | tuple[T, ...], device: str | torch.device
+) -> list[T]: ...
+@overload
+def to_device(tensor: T, device: str | torch.device) -> T: ...
+
+
+def to_device(
+    tensor: T | list[T] | tuple[T, ...], device: str | torch.device
+) -> list[T] | T:
+    """Move tensor(s) to chosen device"""
+    if isinstance(tensor, (list, tuple)):
+        return [x.to(device, non_blocking=True) for x in tensor]
+    return tensor.to(device, non_blocking=True)
