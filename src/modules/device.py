5f247a13fcb9b47f342964c1c8cc2cc45f53967d
Eanspick <ericanepomuceno92@gmail.com>
2024-07-10 08:21:53 +0800

    Move class

--- src/modules/device.py
+++ src/modules/device.py
@@ -1,10 +1,12 @@
-from typing import overload
+from typing import Generic, overload
 
 import torch
 import torch.nn as nn
+from torch.utils.data import DataLoader
 from typing_extensions import TypeVar
 
 T = TypeVar("T", bound=torch.Tensor | nn.Module)
+T_co = TypeVar("T_co", covariant=True, default=torch.Tensor)
 
 
 def get_default_device():
@@ -33,3 +35,21 @@ def to_device(
     if isinstance(tensor, (list, tuple)):
         return [x.to(device, non_blocking=True) for x in tensor]
     return tensor.to(device, non_blocking=True)
+
+
+class DeviceDataLoader(Generic[T_co]):
+    """Wrap a dataloader to move data to a device"""
+
+    def __init__(self, dl: DataLoader[T_co], device: str | torch.device):
+        self.dl = dl
+        self.device = device
+
+    def __iter__(self):
+        """Yield a batch of data after moving it to device"""
+        b: list[torch.Tensor]
+        for b in self.dl:
+            yield to_device(b, self.device)
+
+    def __len__(self):
+        """Number of batches"""
+        return len(self.dl)
